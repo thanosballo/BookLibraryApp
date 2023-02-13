1cbf6bb3c89fad1d7158eef60b9308a22f9b0be2
thanosbalo <thanosbalogiannis@gmail.com>
2023-02-13 23:44:40 +0200

    changes made

--- app.js
+++ app.js
@@ -9,24 +9,45 @@ class Book {
     }
 }
 const dummyBook = new Book("Lord of the rings", "Tolkin", 690, true);
+const dadaBook = new Book("War and peace", "Tolstoi", 222, true);
 console.log(dummyBook);
 const showcase = document.querySelector(".showcase");
-const card = document.createElement("div");
-card.classList.add("card");
-showcase.appendChild(card);
-const info_title = document.createElement("p");
-info_title.textContent = dummyBook.title;
-const info_author = document.createElement("p");
-info_author.textContent = dummyBook.author;
-const info_pages = document.createElement("p");
-info_pages.textContent = dummyBook.pages.toString();
-card.appendChild(info_title);
-card.appendChild(info_author);
-card.appendChild(info_pages);
+createBookCard(dummyBook);
+createBookCard(dadaBook);
+myLibrary.push(dummyBook);
+myLibrary.push(dadaBook);
+console.log(myLibrary);
+function createBookCard(a) {
+    let cardtitle = a.title;
+    let cardauthor = a.author;
+    let cardpages = a.pages;
+    const card = document.createElement("div");
+    card.classList.add("card");
+    showcase.appendChild(card);
+    const info_title = document.createElement("p");
+    info_title.textContent = cardtitle;
+    const info_author = document.createElement("p");
+    info_author.textContent = cardauthor;
+    const info_pages = document.createElement("p");
+    info_pages.textContent = cardpages.toString();
+    card.appendChild(info_title);
+    card.appendChild(info_author);
+    card.appendChild(info_pages);
+}
+;
+function arrayLoop(array) {
+    array.forEach(function (Book) {
+        console.log(Book);
+    });
+}
+;
 const button = document.querySelector("button");
 button.addEventListener("click", () => {
     let domtitle = document.querySelector("#textInput").value;
     let domauthor = document.querySelector("#authorInput").value;
     let dompages = document.querySelector("#numberInput").value;
-    console.log(domtitle, domauthor, dompages);
+    let newBook = new Book(domtitle, domauthor, +dompages, true);
+    createBookCard(newBook);
+    myLibrary.push(newBook);
+    arrayLoop(myLibrary);
 });
--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-let myLibrary=[];
+let myLibrary:Book[]=[];
 class Book{
     title:string;
     author:string;
@@ -11,24 +11,40 @@ constructor (title:string,author:string,pages:number,status:boolean) {
     this.status=status;
 }
 }
-const dummyBook = new Book("Lord of the rings","Tolkin",690,true);
 
+const dummyBook = new Book("Lord of the rings", "Tolkin", 690, true);
+const dadaBook = new Book("War and peace","Tolstoi",222,true);
 console.log(dummyBook);
+const showcase = document.querySelector(".showcase")!;
+createBookCard(dummyBook);
+createBookCard(dadaBook);
+myLibrary.push(dummyBook);
+myLibrary.push(dadaBook);
+console.log(myLibrary);
+function createBookCard(a:Book) {
+    let cardtitle = a.title;
+    let cardauthor = a.author;
+    let cardpages = a.pages;
+    const card = document.createElement("div");
+    card.classList.add("card");
+    showcase.appendChild(card);
+    const info_title = document.createElement("p");
+    info_title.textContent = cardtitle;
+    const info_author = document.createElement("p");
+    info_author.textContent = cardauthor;
+    const info_pages = document.createElement("p");
+    info_pages.textContent = cardpages.toString();
+    card.appendChild(info_title);
+    card.appendChild(info_author);
+    card.appendChild(info_pages);
+}
+;
 
-
-const showcase=document.querySelector(".showcase")!;
-const card=document.createElement("div");
-card.classList.add("card");
-showcase.appendChild(card);
-const info_title=document.createElement("p");
-info_title.textContent=dummyBook.title;
-const info_author=document.createElement("p");
-info_author.textContent=dummyBook.author;
-const info_pages=document.createElement("p");
-info_pages.textContent=dummyBook.pages.toString();
-card.appendChild(info_title);
-card.appendChild(info_author);
-card.appendChild(info_pages);
+function arrayLoop(array:Book[]){
+array.forEach(function (Book){
+console.log(Book);
+})
+};
 
 
 const button=document.querySelector("button")!;
@@ -36,5 +52,8 @@ button.addEventListener("click",()=>{
     let domtitle = (document.querySelector("#textInput") as HTMLInputElement).value;
     let domauthor = (document.querySelector("#authorInput") as HTMLInputElement).value;
     let dompages = (document.querySelector("#numberInput") as HTMLInputElement).value;
-    console.log(domtitle,domauthor,dompages);
+    let newBook=new Book(domtitle,domauthor,+dompages,true);
+    createBookCard(newBook);
+    myLibrary.push(newBook);
+    arrayLoop(myLibrary);
 })
